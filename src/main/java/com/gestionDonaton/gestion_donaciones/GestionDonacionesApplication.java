810d0b029dd6f0b2219609eb827f9204bdd4d930
camilo2046M <ca.menav@duocuc.cl>
2026-05-03 12:48:09 -0400

    initial commit: Inicialización del proyecto y configuración base de pom.xml

---
+++ src/main/java/com/gestionDonaton/gestion_donaciones/GestionDonacionesApplication.java
@@ -0,0 +1,13 @@
+package com.gestionDonaton.gestion_donaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDonacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDonacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionDonaton/gestion_donaciones/GestionDonacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionDonaton.gestion_donaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDonacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
