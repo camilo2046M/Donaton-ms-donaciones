69857fc158f3b0e9b399558a75cd235e43dd16e5
esquizo <ma.lorcam@duocuc.cl>
2026-05-08 16:30:55 -0400

    modificado el aplication y base de datos

--- src/main/java/com/gestionDonaton/gestion_donaciones/GestionDonacionesApplication.java
+++ src/main/java/com/gestionDonaton/gestion_donaciones/GestionDonacionesApplication.java
@@ -2,12 +2,20 @@ package com.gestionDonaton.gestion_donaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.gestionDonaton.gestion_donaciones.repository")
+
+@EntityScan(basePackages = "com.gestionDonaton.gestion_donaciones.model")
 public class GestionDonacionesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GestionDonacionesApplication.class, args);
+		System.out.println(">>> Microservicio de Gestión de Donaciones de Donaton operativo en puerto 8081");
 	}
 
 }
