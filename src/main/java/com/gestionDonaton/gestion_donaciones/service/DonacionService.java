ca31cf3cd523faacebcd23e5100d4fc6a1f6d4b9
esquizo <ma.lorcam@duocuc.cl>
2026-05-08 15:52:25 -0400

    cambios menores en el controlador y en el servicio, ademas de configuracion de la base de datos mediante yaml

--- src/main/java/com/gestionDonaton/gestion_donaciones/service/DonacionService.java
+++ src/main/java/com/gestionDonaton/gestion_donaciones/service/DonacionService.java
@@ -1,20 +1,36 @@
 package com.gestionDonaton.gestion_donaciones.service;
 
-import com.gestionDonaton.gestion_donaciones.model.Donacion;
 import com.gestionDonaton.gestion_donaciones.factory.DonacionFactory;
+import com.gestionDonaton.gestion_donaciones.model.Donacion;
+import com.gestionDonaton.gestion_donaciones.model.DonacionEmpresarial;
 import com.gestionDonaton.gestion_donaciones.repository.DonacionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class DonacionService {
+
     @Autowired
     private DonacionRepository repository;
 
-    public Donacion guardarDonacion(String tipo, Double monto, String nombre) {
-        Donacion d = DonacionFactory.crearDonacion(tipo);
-        d.setMonto(monto);
-        d.setDonanteNombre(nombre);
-        return repository.save(d);
+    public Donacion registrarDonacion(String tipo, Double monto, String nombre, String rut, String certificado) {
+
+        Donacion donacion = DonacionFactory.crearDonacion(tipo);
+
+        donacion.setMonto(monto);
+        donacion.setDonanteNombre(nombre);
+
+
+        if (donacion instanceof DonacionEmpresarial && rut != null) {
+            ((DonacionEmpresarial) donacion).setRutEmpresa(rut);
+            ((DonacionEmpresarial) donacion).setCertificadoImpuestos(certificado);
+        }
+
+        return repository.save(donacion);
+    }
+
+    public List<Donacion> listarTodas() {
+        return repository.findAll();
     }
 }
